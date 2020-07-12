5acb5d3fc9e7a8618c819877c49ef96b0a3df281
Weiyan Zhu <weiyanz@uci.edu>
2020-07-12 12:05:05 -0700

    create files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom"
 import './App.css';
+import Navbar from "./components/layout/Navbar";
+import SelfDescription from "./components/layout/SelfDescription";
+
+import Home from "./components/pages/Home"
+import About from "./components/pages/About"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div className = "container">
+          <SelfDescription/>
+          <Navbar/>
+          <Switch>
+            <Route exact path = "/" component={Home}/>
+            <Route exact path="/About" component={About}/>
+            {/*<Route component={NotFound}/>*/}
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/layout/SelfDescription.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react'
+
+const SelfDescription = () => {
+    return (
+        <Fragment clasName = "text-center">
+            <h1>Weiyan Zhu</h1>
+            <pre>Computer Science{"\n"}
+            Master's student at University of California, Berkeley</pre>
+        </Fragment>
+    )
+}
+
+export default SelfDescription
