f1f92197329e1702eb587be1e82dd64f1b799bf7
Weiyan Zhu <weiyanz@uci.edu>
2020-07-12 16:03:14 -0700

    update NavBar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, {useState} from 'react'
+import {NavLink} from "react-router-dom"
 
 const Navbar = () => {
+    function useForceUpdate(){
+        const [index, setIndex] = useState(1); // integer state
+        return () => setIndex(index==1?2:1); // update the state to force render
+    }
+    const forceUpdate = useForceUpdate();
+
     return (
         <ul className="navigation">
-            <li><Link to = "/">Home</Link></li>
-            <li><Link to = "/About">About</Link></li>
+            <li className={window.location.pathname==="/"?"currentPage":undefined}><NavLink to = "/" onClick={forceUpdate}>Home</NavLink></li>
+            <li className={window.location.pathname==="/About"?"currentPage":undefined}><NavLink to = "/About" onClick={forceUpdate}>About</NavLink></li>
         </ul>
 
     )
--- src/components/layout/SelfDescription.js
+++ src/components/layout/SelfDescription.js
@@ -2,7 +2,7 @@ import React, {Fragment} from 'react'
 
 const SelfDescription = () => {
     return (
-        <Fragment clasName = "text-center">
+        <Fragment>
             <h1 style={{marginLeft:"-5px"}}><strong>Weiyan Zhu</strong></h1>
             <p style={{lineHeight:"0px"}}>Computer Science</p>
             <p style={{lineHeight:"0px"}}>Master's student at University of California, Berkeley</p>
