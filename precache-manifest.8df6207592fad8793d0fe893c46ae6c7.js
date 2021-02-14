8d9087f6d55172c0c25531292609f33d6e973c0d
Weiyan Zhu <weiyanz@berkeley.edu>
2021-02-14 00:16:37 -0800

    Updates

---
+++ precache-manifest.8df6207592fad8793d0fe893c46ae6c7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568ede972ccf0053e3f049d113463397",
+    "url": "/www.weiyanz.com/index.html"
+  },
+  {
+    "revision": "1cf3bc5e3b0ca818fa82",
+    "url": "/www.weiyanz.com/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c9db8cf520555c96c0b3",
+    "url": "/www.weiyanz.com/static/css/main.ee13606f.chunk.css"
+  },
+  {
+    "revision": "1cf3bc5e3b0ca818fa82",
+    "url": "/www.weiyanz.com/static/js/2.3a68e885.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/www.weiyanz.com/static/js/2.3a68e885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9db8cf520555c96c0b3",
+    "url": "/www.weiyanz.com/static/js/main.11c1454a.chunk.js"
+  },
+  {
+    "revision": "3e635350f6d1e7ccfab5",
+    "url": "/www.weiyanz.com/static/js/runtime-main.ff23ccde.js"
+  },
+  {
+    "revision": "bad0197b1a126e46cd833382cf0cca31",
+    "url": "/www.weiyanz.com/static/media/ChibiInu.bad0197b.gif"
+  },
+  {
+    "revision": "3d7f9cbfc4819b8f57f862341f1f4000",
+    "url": "/www.weiyanz.com/static/media/ChristmasGround.3d7f9cbf.png"
+  },
+  {
+    "revision": "01a9b7ac6c98e6cfd8a1c1069aaaeb1a",
+    "url": "/www.weiyanz.com/static/media/ChristmasTree.01a9b7ac.png"
+  },
+  {
+    "revision": "e63d7c317a251c083649ffa2c0c765a4",
+    "url": "/www.weiyanz.com/static/media/aloneTogether.e63d7c31.gif"
+  },
+  {
+    "revision": "402035cfceb4a5254954522bafd6c51c",
+    "url": "/www.weiyanz.com/static/media/box1.402035cf.PNG"
+  },
+  {
+    "revision": "c081ae657e6a2217418d558a94cd196f",
+    "url": "/www.weiyanz.com/static/media/broken compass.c081ae65.gif"
+  },
+  {
+    "revision": "76d8de6173d6f41a86318d9820e22fe4",
+    "url": "/www.weiyanz.com/static/media/harmony.76d8de61.gif"
+  },
+  {
+    "revision": "dbf6584ee3d1667bb704c70b32e40f64",
+    "url": "/www.weiyanz.com/static/media/intershellar.dbf6584e.gif"
+  },
+  {
+    "revision": "fd993c693ed3c21107aa4b0f87d8952b",
+    "url": "/www.weiyanz.com/static/media/lobster.fd993c69.gif"
+  },
+  {
+    "revision": "d17210e1d88ec173fda222704639da0a",
+    "url": "/www.weiyanz.com/static/media/octopus.d17210e1.gif"
+  },
+  {
+    "revision": "5b6806e72b481de74c86412682af1fba",
+    "url": "/www.weiyanz.com/static/media/paintTool.5b6806e7.gif"
+  },
+  {
+    "revision": "f62974e462149aade29a4ec8ea0d7355",
+    "url": "/www.weiyanz.com/static/media/profile.f62974e4.png"
+  },
+  {
+    "revision": "fe6683885347ec93231d23eb8d20ab60",
+    "url": "/www.weiyanz.com/static/media/profileChristmas.fe668388.png"
+  },
+  {
+    "revision": "edfca4f05637a032942e0fb33c5f016b",
+    "url": "/www.weiyanz.com/static/media/riley.edfca4f0.gif"
+  },
+  {
+    "revision": "2f5652c204331d228819ba9aa86031a5",
+    "url": "/www.weiyanz.com/static/media/tree.2f5652c2.png"
+  }
+]);
